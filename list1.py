e850c0584c659d3261571bdea9bf100bb3a98e1b
AKASH <vipulgadekar04@gmail.com>
2023-03-03 16:42:32 +0530

    list duplicate element remove

---
+++ list1.py
@@ -0,0 +1,20 @@
+def findSum(arr, n):
+    # sort all elements of array
+    arr.sort()
+
+    sum = arr[0]
+    for i in range(0, n - 1):
+        if (arr[i] != arr[i + 1]):
+            sum = sum + arr[i + 1]
+
+    return sum
+
+def main():
+    arr = [1, 2, 3, 1, 1, 4, 5, 6]
+    n = len(arr)
+    print(findSum(arr, n))
+
+
+if __name__ == '__main__':
+    main()
+# This code is contributed by 29AjayKumar
